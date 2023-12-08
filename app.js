cb562eb08f9f3eb48330932252ca4ec0b99e53a0
jmateos-incluit <joaquin.mateos@incluit.com>
2023-12-08 11:44:12 -0300

    .

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import router from './routes/index.routes.js';
+import errorController from './controllers/error/index.controller.js';
 
 dotenv.config();
 const { HTTP_PORT } = process.env;
 const app = express();
 
 app.use(router);
+
+app.use(errorController)
 
 app.listen(HTTP_PORT, () =>{
     console.log(`Server running on http://localhost:${HTTP_PORT}`)
